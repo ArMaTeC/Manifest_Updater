86e4ffe07cc9cd7045177d08ac6bba6d2ea623e8
ArMaTeC <armatec0@gmail.com>
2019-09-23 15:09:34 +0100

    updated to the correct version thanks SuperDaniel for spotting that

--- src/Module1.vb
+++ src/Module1.vb
@@ -1,24 +1,18 @@
 ﻿Imports System.IO
 Module Module1
     Sub Main()
-        Dim newmanifest As String = "resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'"
+        Dim newmanifest As String = "resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'"
         Dim list As String()
         Dim dataDirectory As String = String.Format("{0}\", Environment.CurrentDirectory)
-        MsgBox(dataDirectory)
-
         list = Directory.GetFiles(dataDirectory, "__resource.lua", SearchOption.AllDirectories)
         For Each file As String In list
             Dim sb As New Text.StringBuilder()
             Dim myFilePath As String = file
             Dim mySearchToken As String = "resource_manifest_version"
-            Dim foundmanifestdefine As Boolean = False
             For Each line As String In IO.File.ReadLines(myFilePath)
                 If (String.Compare(Left(line, mySearchToken.Length), mySearchToken, True) <> 0) Then
                     'keep other lines
                     sb.AppendLine(line)
-                Else
-                    'remove all old defined manifests
-                    foundmanifestdefine = True
                 End If
             Next
             sb.Insert(0, newmanifest + vbNewLine)
