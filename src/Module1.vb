22b816cc8ef23c69e4d946840524f881fdd2127c
ArMaTeC <armatec0@gmail.com>
2019-09-24 09:59:12 +0100

    added debug output

--- src/Module1.vb
+++ src/Module1.vb
@@ -2,29 +2,29 @@
 Module Module1
     Sub Main()
 
-        Dim newmanifest As String = "resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'"
+        Dim newmanifest As String = "05cfa83c-a124-4cfa-a768-c24a5811d8f9"
         Dim intInput As Integer = 0
         Do
             Integer.TryParse(doMenu, intInput)
             Select Case intInput
                 Case 1
-                    newmanifest = "resource_manifest_version '00000000-0000-0000-0000-000000000000'"
+                    newmanifest = "00000000-0000-0000-0000-000000000000"
                     Exit Do
                     Exit Select
                 Case 2
-                    newmanifest = "resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'"
+                    newmanifest = "77731fab-63ca-442c-a67b-abc70f28dfa5"
                     Exit Do
                     Exit Select
                 Case 3
-                    newmanifest = "resource_manifest_version 'f15e72ec-3972-4fe4-9c7d-afc5394ae207'"
+                    newmanifest = "f15e72ec-3972-4fe4-9c7d-afc5394ae207"
                     Exit Do
                     Exit Select
                 Case 4
-                    newmanifest = "resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'"
+                    newmanifest = "44febabe-d386-4d18-afbe-5e627f4af937"
                     Exit Do
                     Exit Select
                 Case 5
-                    newmanifest = "resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'"
+                    newmanifest = "05cfa83c-a124-4cfa-a768-c24a5811d8f9"
                     Exit Do
                     Exit Select
                 Case 6
@@ -32,22 +32,29 @@ Module Module1
                     Exit Sub
             End Select
         Loop
+        Console.Clear()
         Dim list As String()
+        Dim i As Integer = 0
+
         Dim dataDirectory As String = String.Format("{0}\", Environment.CurrentDirectory)
         list = Directory.GetFiles(dataDirectory, "__resource.lua", SearchOption.AllDirectories)
         For Each file As String In list
             Dim sb As New Text.StringBuilder()
             Dim myFilePath As String = file
             Dim mySearchToken As String = "resource_manifest_version"
+            Console.WriteLine(i.ToString + ") " + file + vbNewLine)
             For Each line As String In IO.File.ReadLines(myFilePath)
                 If (String.Compare(Left(line, mySearchToken.Length), mySearchToken, True) <> 0) Then
                     'keep other lines
                     sb.AppendLine(line)
                 End If
             Next
-            sb.Insert(0, newmanifest + vbNewLine)
+            i += 1
+            sb.Insert(0, "resource_manifest_version '" + newmanifest + "'" + vbNewLine)
             IO.File.WriteAllText(myFilePath, sb.ToString())
         Next
+        Console.WriteLine("All done added/replaced " + i.ToString)
+        Dim s As ConsoleKeyInfo = Console.ReadKey
     End Sub
     Function doMenu() As Char
         Console.Clear()
